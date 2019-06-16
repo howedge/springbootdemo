b5f8d68fbc3e1505a0e4d56eb60cb14dcb1b1b28
howedge <howedge@126.com>
2019-06-16 17:08:18 +0800

    new springboot demo

---
+++ src/main/java/com/howedge/springbootdemo/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.howedge.springbootdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello thj from docker";
+    }
+}
